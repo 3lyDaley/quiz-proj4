09a1eddc7035a579f6fca95bedc4890a969e6f99
Bad Boys Club <3ly.daley@gmail.com>
2022-04-24 16:21:37 -0700

    changes from seinfeld to API fed quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,36 @@
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-document.getElementById('startButton').onclick = function() {
-  location.href = "page2.html"
-};
+var qCard = document.getElementById('data-quiz-item')
+var qCardChoices = document.getElementById('mChoice')
+function getApi() {
+  
+  var requestUrl = 'https://opentdb.com/api.php?amount=12&category=19&difficulty=medium&type=multiple';
+  
+  fetch(requestUrl)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    // TODO: Loop through the response
+    for (var i = 0; i < 12; i++){
+      console.log(data.results[i].question);
+      
+      var answerChoices = [];
+      var question = document.createElement('h4');
+      question.innerHTML = data.results[i].question;
 
-var buildQuiz = function(){
+        qCard.append(question);
 
+        answerChoices.push(data.results[i].correct_answer, data.results[i].incorrect_answers);
+        console.log(answerChoices)
+
+        
+
+
+      }
+    });
 }
+// document.getElementById('startButton').onclick = function() {
+//   location.href = "page2.html"
+// };
 
-var showResults = function() {
+getApi();
 
-}
-
-
-// on submit, show results
-submitButton.addEventListener("click", showResults);
